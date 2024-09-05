2d78b64a7d720b826dca3c59551666a7a4edb125
Tony <tony@soundablehealth.com>
2024-09-05 21:13:31 +0900

    first commit

---
+++ blog/2024-09-05-first-blog-post.md
@@ -0,0 +1,12 @@
+---
+slug: first-blog-post
+title: To be updated
+authors: [Tony]
+tags: []
+---
+
+To be updated...
+
+<!-- truncate -->
+
+...consectetur adipiscing elit. Pellentesque elementum dignissim ultricies. Fusce rhoncus ipsum tempor eros aliquam consequat. Lorem ipsum dolor sit amet
---
+++ docusaurus.config.ts
@@ -0,0 +1,108 @@
+import {themes as prismThemes} from 'prism-react-renderer';
+import type {Config} from '@docusaurus/types';
+import type * as Preset from '@docusaurus/preset-classic';
+
+const config: Config = {
+  title: 'Soundable',
+  tagline: '',
+  favicon: 'img/favicon.ico',
+
+  // Set the production url of your site here
+  url: 'https://www.soundable.health',
+  // Set the /<baseUrl>/ pathname under which your site is served
+  // For GitHub pages deployment, it is often '/<projectName>/'
+  baseUrl: '/',
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: 'SoundableHealth', // Usually your GitHub org/user name.
+  projectName: 'soundable-docs', // Usually your repo name.
+
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+
+  // Even if you don't use internationalization, you can use this field to set
+  // useful metadata like html lang. For example, if your site is Chinese, you
+  // may want to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en'],
+  },
+
+  presets: [
+    [
+      'classic',
+      {
+        docs: {
+          sidebarPath: './sidebars.ts',
+          routeBasePath: '/', // Serve the docs at the site's root
+        },
+        blog: false, // Optional: disable the blog plugin
+        pages: false, // Optional: disable the pages plugin
+        theme: {
+          customCss: './src/css/custom.css',
+        },
+      } satisfies Preset.Options,
+    ],
+  ],
+
+  themeConfig: {
+    // Replace with your project's social card
+    image: 'img/soundable-logo.jpg',
+    navbar: {
+      title: 'Soundable',
+      logo: {
+        alt: 'soundable',
+        src: 'img/logo.svg',
+        href: '/',
+      },
+      items: [
+        {
+          type: 'docSidebar',
+          sidebarId: 'tutorialSidebar',
+          position: 'left',
+          label: 'Development',
+        },
+        {
+          href: 'https://www.soundable.health',
+          label: 'Soundable Health',
+          position: 'right',
+        },
+      ],
+    },
+    footer: {
+      style: 'dark',
+      links: [
+        {
+          title: 'Docs',
+          items: [
+            {
+              label: 'Docs',
+              to: '/',
+            },
+          ],
+        },
+        {
+          title: 'More',
+          items: [
+            {
+              label: 'Terms of Use',
+              href: 'https://www.soundable.health/terms-of-use',
+            },
+            {
+              label: 'Privacy Policy',
+              href: 'https://www.soundable.health/privacy-policy',
+            },
+          ],
+        },
+      ],
+      copyright: `Copyright © ${new Date().getFullYear()} Soundable Health, Inc.`,
+    },
+    prism: {
+      theme: prismThemes.github,
+      darkTheme: prismThemes.dracula,
+    },
+  } satisfies Preset.ThemeConfig,
+};
+
+export default config;
